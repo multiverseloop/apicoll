88417d82e3875a47214058001730f60cb3771106
ShashankMisra <shashankmisra@fico.com>
2022-01-22 20:35:29 +0530

    update the code

--- .aws-sam/build/App/src/Lex/postcontent.js
+++ .aws-sam/build/App/src/Lex/postcontent.js
@@ -2,12 +2,12 @@ var AWS = require("aws-sdk");
 
 console.log("Inside the post content");
 
+var lexruntime = new AWS.LexRuntime();
+
 AWS.config.update({
   region: "eu-central-1",
   endpoint: "https://dynamodb.eu-central-1.amazonaws.com"
 });
-
-var lexruntime = new AWS.LexRuntime();
   
 async function postContent(req) {
 console.log(req.userId)
@@ -22,33 +22,14 @@ var params = {
     contentType: 'text/plain; charset=utf-8', /* required */    
     userId: req.userId, /* required */
     accept: 'text/plain; charset=utf-8',
-    
+    inputText: "I already made a payment"    
   };
   
-  try{
-    console.log("are you here")
-    const data = await lexruntime.postText(params)
-    console.log(data.intentName)
-    if(data.Item == undefined) {
-      console.log("Did not find existing Login with the email. Hence returning NOT FOUND")
-      return JSON.stringify({
-        collectionsStatus:"NOT_FOUND",
-        resolvedStatus:false,
-        resolvedData:""
-      })
-    }
-    else {
-      console.log("its your lucky day. Found the email")
-      return JSON.stringify({
-                  collectionsStatus:data.Item.collectionsStatus,
-                  resolvedStatus:data.Item.resolvedStatus,
-                  resolvedData:data.Item.resolvedData
-      })
-    }
-    } catch (err) { 
-        console.log("Final errir "+err)
-    return err
-  }
+  lexruntime.postText(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+  
  
 
 }
